f4eade2080bc97a62fd73da8f412352ec6a15801
Artur <info@aigroupusa.com>
2021-01-18 21:23:17 -0500

    commit tree 9986081e2e6665f38e953d50bf61974e94c3202b

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+    
+    if array.length == 1
+        array.join
+    
+    elsif array.length == 2
+        array << "and "+ array.pop
+      array.join(" ")
+    
+    else array.length > 2
+      array << "and "+ array.pop
+      array.join(", ")
+     
+    end
+        
 end
